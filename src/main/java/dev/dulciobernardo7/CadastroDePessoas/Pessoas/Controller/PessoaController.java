9f1907c434d3bb177b4e96be2296feaf7ec782d3
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-08-22 11:37:38 +0100

    feat: altera rota do controller para listar Pessoas e injeta dependências do service e repository

--- src/main/java/dev/dulciobernardo7/CadastroDePessoas/Pessoas/Controller/PessoaController.java
+++ src/main/java/dev/dulciobernardo7/CadastroDePessoas/Pessoas/Controller/PessoaController.java
@@ -1,16 +1,51 @@
 package dev.dulciobernardo7.CadastroDePessoas.Pessoas.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.dulciobernardo7.CadastroDePessoas.Pessoas.Model.PessoaModel;
+import dev.dulciobernardo7.CadastroDePessoas.Pessoas.PessoaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/pessoa")
 public class PessoaController {
+    private PessoaService pessoaService;
+
+    public PessoaController(PessoaService pessoaService) {
+        this.pessoaService = pessoaService;
+    }
 
     @GetMapping(  "/boavindas")
     public String boaVindas(){
         return "Seja bem vindo na minha aplicao backend de cadastro de pessoas";
     }
 
+    // Adicionar ninja (CREATE)
+    @PostMapping("/cadastrar")
+    public  String CadastraPessoa(){
+        return "Cadastrada com sucesso";
+    }
+    //Mostrar todos os ninjas (READ)
+    @GetMapping("/lista")
+    public List<PessoaModel> TodasPessoas(){
+        return pessoaService.ListatodasPessoas();
+    }
+
+    //Mostrar ninja por id (READ)
+    @GetMapping("/todoID")
+    public  String MostrarTodasPessoasPorId(){
+        return "Mostrar Todas Pessoas";
+    }
+
+    // Alterar dados dos ninjas (UPDATE)
+    @PostMapping("/alteraID")
+    public String AlteraPorId(){
+        return "Alterar por Id";
+    }
+
+    // Deletar Ninja (DELETE)
+    @DeleteMapping("/DeletarID")
+    public String ExcluirPessoaPorId(){
+        return"Pessoa Deletada por id";
+    }
 }
--- src/main/java/dev/dulciobernardo7/CadastroDePessoas/Pessoas/PessoaService.java
+++ src/main/java/dev/dulciobernardo7/CadastroDePessoas/Pessoas/PessoaService.java
@@ -1,4 +1,26 @@
 package dev.dulciobernardo7.CadastroDePessoas.Pessoas;
 
+import dev.dulciobernardo7.CadastroDePessoas.Pessoas.Model.PessoaModel;
+import dev.dulciobernardo7.CadastroDePessoas.Tarefas.Model.TarefasModel;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PessoaService {
+    private PessoasRepository pessoasRepository;
+
+    public PessoaService(PessoasRepository pessoasRepository){
+        this.pessoasRepository = pessoasRepository;
+    }
+
+    //Metodo para lista todo o pessoas
+    public List<PessoaModel> ListatodasPessoas(){
+        return pessoasRepository.findAll();
+    }
+
+    //Metodo para lista todo as pessoas pelo id
+   // public List<TarefasModel> ListatodasPessoasporId(){
+ //       return pessoasRepository.getReferenceById(1L);
+  //  }
 }
