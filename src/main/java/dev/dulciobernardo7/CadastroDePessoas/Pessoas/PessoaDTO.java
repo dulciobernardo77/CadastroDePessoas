dcd763c252864b479fa77e666a5e0484ee9f7842
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-08-25 15:42:03 +0100

    chore: sincroniza versão do DTO com a entidade e adiciona mapeamento manual no PessoaMapper

--- src/main/java/dev/dulciobernardo7/CadastroDePessoas/Pessoas/PessoaDTO.java
+++ src/main/java/dev/dulciobernardo7/CadastroDePessoas/Pessoas/PessoaDTO.java
@@ -1,4 +1,25 @@
 package dev.dulciobernardo7.CadastroDePessoas.Pessoas;
 
+import dev.dulciobernardo7.CadastroDePessoas.Tarefas.Model.TarefasModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PessoaDTO {
+
+    private Long id;
+    private String nome ;
+    private   int idade;
+    private String nacionalidade;
+    private String bi;
+    private char sexo;
+    private String email;
+    private String telefone;
+    private String nivel;
+    private String imgUrl;
+    private TarefasModel tarefa;
 }
