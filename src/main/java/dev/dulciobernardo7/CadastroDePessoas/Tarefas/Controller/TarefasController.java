c69c19e056c29652ba9fc232088d8521824a6f99
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-08-26 12:30:44 +0100

    refactor: delega transformação de Tarefas ao DTO via Streams no Mapper

--- src/main/java/dev/dulciobernardo7/CadastroDePessoas/Tarefas/Controller/TarefasController.java
+++ src/main/java/dev/dulciobernardo7/CadastroDePessoas/Tarefas/Controller/TarefasController.java
@@ -2,6 +2,7 @@ package dev.dulciobernardo7.CadastroDePessoas.Tarefas.Controller;
 
 import dev.dulciobernardo7.CadastroDePessoas.Pessoas.Model.PessoaModel;
 import dev.dulciobernardo7.CadastroDePessoas.Tarefas.Model.TarefasModel;
+import dev.dulciobernardo7.CadastroDePessoas.Tarefas.TarefasDTO;
 import dev.dulciobernardo7.CadastroDePessoas.Tarefas.TarefasService;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,24 +20,24 @@ public class TarefasController {
 
     // Adicionar ninja (CREATE)
     @PostMapping("/cadastrar")
-    public  String CadastraPessoa(){
-        return "Cadastrada com sucesso";
+    public  TarefasDTO CadastraPessoa(@RequestBody TarefasDTO tarefasDTO){
+        return tarefasService.cadastroDeTarefas(tarefasDTO);
     }
     //Mostrar todos os ninjas (READ)
     @GetMapping("/lista")
-    public List<TarefasModel> TodasPessoas(){
+    public List<TarefasDTO> TodasPessoas(){
         return tarefasService.ListatodasPessoas();
     }
 
     //Mostrar ninja por id (READ)
     @GetMapping("/lista/{number}")
-    public  TarefasModel MostrarTodasPessoasPorId(@PathVariable Long number){
+    public TarefasDTO MostrarTodasPessoasPorId(@PathVariable Long number){
         return tarefasService.ListatodasPessoasPorId(number);
     }
 
     // Alterar dados dos ninjas (UPDATE)
     @PostMapping("/altera/{number}")
-    public TarefasModel AlteraPorId(@PathVariable Long number, @RequestBody TarefasModel tarefas){
+    public TarefasDTO AlteraPorId(@PathVariable Long number, @RequestBody TarefasDTO tarefas){
         return tarefasService.AtualizarTarefas(number,tarefas);
     }
 
