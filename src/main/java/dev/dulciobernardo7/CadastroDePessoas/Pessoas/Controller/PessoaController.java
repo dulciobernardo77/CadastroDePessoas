654a6e1c2377f3abb26772b0c6928b4b868eb495
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-08-26 16:02:04 +0100

    refactor: Refatora o controller para incluir o responseEntity nas minhas rotas e customizar a resposta das requisicoes do cliente

--- src/main/java/dev/dulciobernardo7/CadastroDePessoas/Pessoas/Controller/PessoaController.java
+++ src/main/java/dev/dulciobernardo7/CadastroDePessoas/Pessoas/Controller/PessoaController.java
@@ -1,8 +1,9 @@
 package dev.dulciobernardo7.CadastroDePessoas.Pessoas.Controller;
 
-import dev.dulciobernardo7.CadastroDePessoas.Pessoas.Model.PessoaModel;
+
 import dev.dulciobernardo7.CadastroDePessoas.Pessoas.PessoaDTO;
 import dev.dulciobernardo7.CadastroDePessoas.Pessoas.PessoaService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,33 +23,50 @@ public class PessoaController {
         return "Seja bem vindo na minha aplicao backend de cadastro de pessoas";
     }
 
-    // Adicionar ninja (CREATE)
+    // Adicionar Funcionario (CREATE)
     @PostMapping("/cadastrar")
-    public PessoaDTO CadastraPessoa(@RequestBody PessoaDTO pessoaModel){
-        return pessoaService.cadastroDeFuncionario(pessoaModel);
+    public ResponseEntity<String> CadastraPessoa(@RequestBody PessoaDTO pessoaDTO){
+        PessoaDTO pessoaDTO1 =  pessoaService.cadastroDeFuncionario(pessoaDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Funcionario Cadastrado"+pessoaDTO1.getNome()+" com Sucesso") ;
     }
-     //Mostrar todos os ninjas (READ)
+     //Mostrar todos os Funcionarios (READ)
     @GetMapping("/lista")
     public List<PessoaDTO> TodasPessoas(){
         return pessoaService.ListatodasPessoas();
     }
 
-    //Mostrar ninja por id (READ)
+    //Mostrar Funcionario por id (READ)
     @GetMapping("/lista/{number}")
-    public  PessoaDTO MostrarTodasPessoasPorId(@PathVariable Long number){
-        return pessoaService.ListatodasPessoasporId(number);
+    public  ResponseEntity<String> MostrarTodasPessoasPorId(@PathVariable Long number){
+
+          PessoaDTO pessoaDTO =  pessoaService.ListatodasPessoasporId(number);
+          if (pessoaDTO != null){
+              return ResponseEntity.ok("Funcionario encontrado: "+pessoaDTO.getNome());
+          }else {
+              return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O Funcionario com IDs "+number+" Nao encontrado");
+
+          }
     }
 
-    // Alterar dados dos ninjas (UPDATE)
+    // Alterar dados dos Funcionario (UPDATE)
     @PutMapping("/alterar/{number}")
-    public PessoaDTO AlteraPorId(@PathVariable Long number,@RequestBody PessoaDTO pessoa){
-        return pessoaService.Atualizarfuncionario(number, pessoa);
+    public ResponseEntity<?> AlteraPorId(@PathVariable Long number,@RequestBody PessoaDTO pessoa){
+        PessoaDTO pessoaDTO = pessoaService.Atualizarfuncionario(number, pessoa);
+        if (pessoaDTO != null) {
+            return ResponseEntity.ok(pessoaDTO);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O Funcionario com IDs "+number+" Nao encontrado");
+        }
     }
 
-    // Deletar Ninja (DELETE)
+    // Deletar Funcionario (DELETE)
     @DeleteMapping("/deletar/{number}")
-    public ResponseEntity<Void> ExcluirPessoaPorId(@PathVariable Long number ){
-         pessoaService.ExcluirPessoaPorId(number);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<String> ExcluirPessoaPorId(@PathVariable Long number ){
+        if (pessoaService.ListatodasPessoasporId(number)!= null) {
+            pessoaService.ExcluirPessoaPorId(number);
+            return ResponseEntity.ok("O funcionario  com o IDs "+ number+" Excluido.");
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O ninja com IDs "+number+" Nao encontrado");
+        }
     }
 }
