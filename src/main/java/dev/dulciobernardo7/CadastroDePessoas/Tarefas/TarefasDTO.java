e98f9dc348c3d1ffb31634e55cef89f66199aa79
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-05 15:22:30 +0100

    refactor: adiciona builder ao DTO de tarefas
    Inclui a anotacao Lombok @Builder em TarefasDTO para permitir a criacao fluente dos dados de tarefas e preparar o DTO para o mapeamento padronizado.

--- src/main/java/dev/dulciobernardo7/CadastroDePessoas/Tarefas/TarefasDTO.java
+++ src/main/java/dev/dulciobernardo7/CadastroDePessoas/Tarefas/TarefasDTO.java
@@ -2,11 +2,13 @@ package dev.dulciobernardo7.CadastroDePessoas.Tarefas;
 
 import dev.dulciobernardo7.CadastroDePessoas.Pessoas.PessoaModel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
