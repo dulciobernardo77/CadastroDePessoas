127352b7d11d3ea4a2a885b3557b3656895b7b4b
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-05 15:18:54 +0100

    refactor: usa builder no mapper de pessoas
    Substitui a montagem manual de PessoaModel e PessoaDTO por chamadas ao builder, mantendo todos os campos mapeados e tornando a conversao entre entidade e DTO mais legivel.

--- src/main/java/dev/dulciobernardo7/CadastroDePessoas/Pessoas/PessoaMapper.java
+++ src/main/java/dev/dulciobernardo7/CadastroDePessoas/Pessoas/PessoaMapper.java
@@ -1,5 +1,6 @@
 package dev.dulciobernardo7.CadastroDePessoas.Pessoas;
 
+import lombok.experimental.UtilityClass;
 import org.springframework.stereotype.Component;
 
 
@@ -7,36 +8,36 @@ import org.springframework.stereotype.Component;
 public class PessoaMapper {
 
     public PessoaModel map(PessoaDTO pessoaDTO){
-        PessoaModel pessoaModel = new PessoaModel();
-        pessoaModel.setId(pessoaDTO.getId());
-        pessoaModel.setNome(pessoaDTO.getNome());
-        pessoaModel.setIdade(pessoaDTO.getIdade());
-        pessoaModel.setNacionalidade(pessoaDTO.getNacionalidade());
-        pessoaModel.setBi(pessoaDTO.getBi());
-        pessoaModel.setSexo(pessoaDTO.getSexo());
-        pessoaModel.setEmail(pessoaDTO.getEmail());
-        pessoaModel.setTelefone(pessoaDTO.getTelefone());
-        pessoaModel.setNivel(pessoaDTO.getNivel());
-        pessoaModel.setImgUrl(pessoaDTO.getImgUrl());
-        pessoaModel.setTarefa(pessoaDTO.getTarefa());
-        return pessoaModel;
+        return PessoaModel
+                .builder()
+                .id(pessoaDTO.getId())
+                .nome(pessoaDTO.getNome())
+                .idade(pessoaDTO.getIdade())
+                .nacionalidade(pessoaDTO.getNacionalidade())
+                .bi(pessoaDTO.getBi())
+                .sexo(pessoaDTO.getSexo())
+                .email(pessoaDTO.getEmail())
+                .telefone(pessoaDTO.getTelefone())
+                .nivel(pessoaDTO.getNivel())
+                .imgUrl(pessoaDTO.getImgUrl())
+                .tarefa(pessoaDTO.getTarefa())
+                .build();
     }
 
     public PessoaDTO map(PessoaModel pessoaModel){
-        PessoaDTO pessoaDTO = new PessoaDTO();
-
-        //error
-        pessoaDTO.setId(pessoaModel.getId());
-        pessoaDTO.setNome(pessoaModel.getNome());
-        pessoaDTO.setIdade(pessoaModel.getIdade());
-        pessoaDTO.setNacionalidade(pessoaModel.getNacionalidade());
-        pessoaDTO.setBi(pessoaModel.getBi());
-        pessoaDTO.setSexo(pessoaModel.getSexo());
-        pessoaDTO.setEmail(pessoaModel.getEmail());
-        pessoaDTO.setTelefone(pessoaModel.getTelefone());
-        pessoaDTO.setNivel(pessoaModel.getNivel());
-        pessoaDTO.setImgUrl(pessoaModel.getImgUrl());
-        pessoaDTO.setTarefa(pessoaModel.getTarefa());
-        return pessoaDTO;
+        return PessoaDTO
+                .builder()
+                .id(pessoaModel.getId())
+                .nome(pessoaModel.getNome())
+                .idade(pessoaModel.getIdade())
+                .nacionalidade(pessoaModel.getNacionalidade())
+                .bi(pessoaModel.getBi())
+                .sexo(pessoaModel.getSexo())
+                .email(pessoaModel.getEmail())
+                .telefone(pessoaModel.getTelefone())
+                .nivel(pessoaModel.getNivel())
+                .imgUrl(pessoaModel.getImgUrl())
+                .tarefa(pessoaModel.getTarefa())
+                .build();
     }
 }
